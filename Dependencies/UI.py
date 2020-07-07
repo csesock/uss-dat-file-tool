b553359e7727d171237dc6f71c8838e3fbdd0a8a
csesock <csesock@murraystate.edu>
2020-07-07 14:57:45 -0500

    continued adding functions into the UI

--- Dependencies/UI.py
+++ Dependencies/UI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+import sys
 
 # intializing the window
 window = tk.Tk()
@@ -47,15 +48,38 @@ def fixOfficeRegionZoneFields():
                     zone = line[75:77]
                     if zone == "  ":
                         zone = "BLANK"
-                    print("-------------------------")
-                    print("Office: \t", str(office))
-                    print("Region: \t", str(region))
-                    print("Zone: \t\t", str(zone))
-                    print("-------------------------")
-                    textBox.insert(counter, "Office: \t", str(office) + "\n")
-                    textBox.insert(counter, "Region: \t", str(region) + "\n")
-                    textBox.insert(counter, "Zone: \t", str(zone) + "\n")
+##                    print("-------------------------")
+##                    print("Office: \t", str(office))
+##                    print("Region: \t", str(region))
+##                    print("Zone: \t\t", str(zone))
+##                    print("-------------------------")
+                    textBox.delete(1.0, "end")
+                    textBox.insert(1.0, "Office: \t" + str(office))
+                    textBox.insert(2.0, "\n")
+                    textBox.insert(2.0, "Region: \t" + str(region))
+                    textBox.insert(3.0, "\n")
+                    textBox.insert(3.0, "Zone: \t" + str(zone))
                     break
+    except FileNotFoundError:
+        print("fnf")
+
+def scanAllRecordsVerbose():
+    all_records = {}
+    counter = 1.0
+    try:
+        with open('download.dat', 'r') as openfile:
+            for line in openfile:
+                x = line[0:3]
+                if x not in all_records:
+                    all_records[x] = 1
+                else:
+                    all_records[x]+=1
+            textBox.delete(1.0, "end")
+            for record in all_records:
+                #print(record, " . . . . :\t", f"{all_records[record]:,d}")
+                textBox.insert(counter, str(record) + ". . . :\t" + f"{all_records[record]:,d}")
+                counter+=1
+                textBox.insert(counter, "\n")
     except FileNotFoundError:
         print("fnf")
 
@@ -64,17 +88,22 @@ TAB_CONTROL = ttk.Notebook(window)
 
 #Tab1
 TAB1 = ttk.Frame(TAB_CONTROL)
-TAB_CONTROL.add(TAB1, text='File Operations')
+TAB_CONTROL.add(TAB1, text=' Operations ')
 
 #Tab2
 TAB2 = ttk.Frame(TAB_CONTROL)
-TAB_CONTROL.add(TAB2, text='Visualizations')
+TAB_CONTROL.add(TAB2, text=' Visualizations ')
 TAB_CONTROL.pack(expand=1, fill="both")
 
-#Tab Name Labels
+#Tab3
+TAB3 = ttk.Frame(TAB_CONTROL)
+TAB_CONTROL.add(TAB3, text=" Import/Export ")
+TAB_CONTROL.pack(expand=1, fill="both")
+
+#Tab1 Widgets
 b1 = tk.Button(TAB1, text="1. Single Record Scan")
 b1.place(x=20, y=20)
-b2 = tk.Button(TAB1, text="2. Verbose Record Scan")
+b2 = tk.Button(TAB1, text="2. Verbose Record Scan", command=lambda:scanAllRecordsVerbose())
 b2.place(x=20, y=60)
 b3 = tk.Button(TAB1, text="3. Print Single Record", command=lambda:printAllRecords())
 b3.place(x=20, y=100)
@@ -88,11 +117,61 @@ b7 = tk.Button(TAB1, text="7. Check Malformed Lat/Long")
 b7.place(x=20, y=260)
 
 l = tk.Label(TAB1, text="Console:").place(x=205, y=10)
-textBox = tk.Text(TAB1, height=16, width=55, background='black', foreground='green')
+textBox = tk.Text(TAB1, height=16, width=55, background='black', foreground='lawn green')
 textBox.place(x=210, y=30)
+#S = tk.Scrollbar(window, command=textBox.yview())
 
-ttk.Label(TAB2, text="This is Tab 2").grid(column=0, row=0, padx=10, pady=10)
+#Tab2 Widgets
+tab2label = tk.Label(TAB2, text="Data Visualization")
+tab2label.place(x=20, y=20)
+tab2label2 = tk.Label(TAB2, text="Select Data to Display:")
+tab2label2.place(x=20, y=50)
 
+tab2check1 = tk.Checkbutton(TAB2, text="Customer")
+tab2check1.place(x=30, y=80)
+tab2check2 = tk.Checkbutton(TAB2, text="Route")
+tab2check2.place(x=30, y=100)
+tab2check3 = tk.Checkbutton(TAB2, text="Meter")
+tab2check3.place(x=30, y=120)
+tab2check4 = tk.Checkbutton(TAB2, text="Radio Reads")
+tab2check4.place(x=30, y=140)
+
+canvas = tk.Canvas(TAB2, width=200, height=200)
+canvas.place(x= 250, y=30)
+canvas.create_line(0, 0, 200, 100)
+canvas.create_line(0, 100, 200, 0)
+
+#Tab3 Widgets
+tab3label = tk.Label(TAB3, text="Import/Export data:")
+tab3label.place(x=20, y=20)
+tab3importbutton = tk.Button(TAB3, text="Import Data...")
+tab3importbutton.place(x=20, y=50)
+tab3exportbutton = tk.Button(TAB3, text="Export Data...")
+tab3exportbutton.place(x=110, y=50)
+
+# menu
+menubar = tk.Menu(window)
+
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open")
+filemenu.add_command(label="Save")
+filemenu.add_separator()
+filemenu.add_command(label="Exit")
+menubar.add_cascade(label="File", menu=filemenu)
+
+# create more pulldown menus
+editmenu = tk.Menu(menubar, tearoff=0)
+editmenu.add_command(label="Cut")
+editmenu.add_command(label="Copy")
+editmenu.add_command(label="Paste")
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+helpmenu = tk.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About")
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+# display the menu
+window.config(menu=menubar)
 #Calling Main()
 window.mainloop()
 
