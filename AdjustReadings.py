4821705d42b0f3297f16f71be4dc1dd32124b683
csesock <csesock@murraystate.edu>
2020-11-13 13:51:36 -0600

    continued implementation of adjust readings

--- AdjustReadings.py
+++ AdjustReadings.py
@@ -2,10 +2,10 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk 
 
-def adjustReadingsPopup(download_filename, button_pushed):
+def adjustReadingsPopup(download_filename):
     root = tk.Tk()
     root.title("Adjust Readings Wizard")
-    #root.resizable(False, False)
+    root.resizable(False, False)
     t = ttk.Style(root)
     t.theme_use('clam')
     try:
@@ -13,8 +13,12 @@ def adjustReadingsPopup(download_filename, button_pushed):
         root.iconphoto(False, photo)
     except:
         pass
+    
+    def getRadioButton():
+        r = radio_var.get()
+        print(radio_var.get())
+        print(r)
 
-    # Core functionality
     def adjustReadings():
         # which_button_is_selected = radio_var.get()
         # print(which_button_is_selected)
@@ -31,7 +35,6 @@ def adjustReadingsPopup(download_filename, button_pushed):
                 rff = ""
                 for line in openfile:
                     if line.startswith('RDG'):
-                        #rdg = line[33:43].strip().rstrip()
                         line = line.replace(line[33:43], correct[counter])
                         counter+=1
                     builtfile.write(line)
@@ -61,11 +64,9 @@ def adjustReadingsPopup(download_filename, button_pushed):
     Label(root, text=download_filename[-40:]).place(x=80, y=20)
     Label(root, text="This window will create a new download file by pulling \n the raw read and adjusting the numbers accordingly. \n Specify below how to adjust the readings.").place(x=50, y=50)
 
-    v = tk.StringVar()
     radio_var = IntVar()    
-    Radiobutton(root,text='Config1', value=1,variable = radio_var).place(x=100, y=110)
-    #Radiobutton(root, text="Increment", variable=v, value='1').place(x=100, y=110)
-    Radiobutton(root, text="Decrement", variable=v, value='2').place(x=185, y=110)
+    Radiobutton(root,text='Increment', value=5, variable=radio_var).place(x=100, y=110)
+    Radiobutton(root, text="Decrement", value=6, variable=radio_var).place(x=185, y=110)
 
     Label(root, text="Digits to change:").place(x=105, y=140)
     numToDrop = ttk.Combobox(root, width=6, values = ["0", "1", "2", "3", "4"])
@@ -73,7 +74,7 @@ def adjustReadingsPopup(download_filename, button_pushed):
     numToDrop.state(['readonly'])
     numToDrop.set("1")
 
-    ttk.Button(root, text="Execute", width=10, command=lambda:adjustReadings()).place(x=100, y=180)
+    ttk.Button(root, text="Execute", width=10, command=lambda:getRadioButton()).place(x=100, y=180)
     ttk.Button(root, text="Exit", command=root.destroy, width=10).place(x=190, y=180)
 
     mainloop()
